8ace17b9e57d63010e145335b86b00a7137a4a11
Midwayne <kushal5070@gmail.com>
2026-01-30 20:14:00 +0530

    docs: improved vector docs

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # stdlib
 
 A C standard library implementation featuring common data structures.
+This was created as a fun project while learning C. Some implementations may not be optimal.
 
 ## Data Structures
 
@@ -72,27 +73,6 @@ int main(void) {
 }
 ```
 
-## API Reference
+## Documentation
 
-### Lifecycle
-- `vector_init(v, element_size)` - Initialize with default capacity
-- `vector_reserve(v, capacity)` - Reserve memory for capacity elements
-- `vector_free(v)` - Free all memory
-
-### Access
-- `vector_get(v, index)` - Get element at index
-- `vector_front(v)` - Get first element
-- `vector_back(v)` - Get last element
-
-### Modifiers
-- `vector_set(v, index, element)` - Set element at index
-- `vector_push_back(v, element)` - Add element to end
-- `vector_pop_back(v)` - Remove last element
-- `vector_insert(v, index, element)` - Insert at index
-- `vector_erase(v, index)` - Remove at index
-- `vector_clear(v)` - Remove all elements
-- `vector_shrink_to_fit(v)` - Reduce capacity to size
-
-### Status
-- `vector_size(v)` - Get number of elements
-- `vector_is_empty(v)` - Check if empty
+For detailed API documentation, see the [docs/](docs/) directory.
--- include/vector.h
+++ include/vector.h
@@ -19,61 +19,115 @@ typedef struct {
 
 /* Lifecycle management */
 
-// Initialise a vector with default capacity
+/*
+ * Initialise a vector with default capacity
+ * @param v Pointer to the Vector to initialise
+ * @param element_size Size of each element in bytes
+ */
 void vector_init(Vector *v, size_t element_size);
 
-// Reserve a specific amount of memory for the vector (should be used after initialisation)
+/*
+ * Reserve a specific amount of memory for the vector (should be used after initialisation)
+ * @param v Pointer to the Vector
+ * @param capacity Number of elements to reserve space for
+ */
 void vector_reserve(Vector *v, size_t capacity);
 
-// Free memory associated with the vector
+/*
+ * Free memory associated with the vector
+ * @param v Pointer to the Vector to free
+ */
 void vector_free(Vector *v);
 
 
 
 /* Access */
 
-// Gets the element in the vector at the required index
+/*
+ * Gets the element in the vector at the required index
+ * @param v Pointer to the Vector
+ * @param index Index of the element to retrieve
+ */
 void* vector_get(Vector *v, size_t index);
 
-// Returns a pointer to the last element
+/*
+ * Returns a pointer to the last element
+ * @param v Pointer to the Vector
+ */
 void* vector_front(Vector *v);
 
-// Returns a pointer to the first element
+/*
+ * Returns a pointer to the first element
+ * @param v Pointer to the Vector
+ */
 void* vector_back(Vector *v);
 
 
 
 /* Modifiers */
 
-// Assigns the element in the vector at the given index (overwrites the previous element)
+/*
+ * Assigns the element in the vector at the given index (overwrites the previous element)
+ * @param v Pointer to the Vector
+ * @param index Index where the element will be set
+ * @param element Pointer to the element to copy into the vector
+ */
 void vector_set(Vector *v, size_t index, const void* element);
 
-// Assigns the element to the end of the vector
+/*
+ * Assigns the element to the end of the vector
+ * @param v Pointer to the Vector
+ * @param element Pointer to the element to push back
+ */
 void vector_push_back(Vector *v, const void* element);
 
-// Decrements the size of the vector (It will not free the memory)
+/*
+ * Decrements the size of the vector (It will not free the memory)
+ * @param v Pointer to the Vector
+ */
 void vector_pop_back(Vector *v);
 
-// Assigns the element in the vector at the given index (shifts all the existing elements to the right)
+/*
+ * Assigns the element in the vector at the given index (shifts all the existing elements to the right)
+ * @param v Pointer to the Vector
+ * @param index Index where the element will be inserted
+ * @param element Pointer to the element to insert
+ */
 void vector_insert(Vector *v, size_t index, const void* element);
 
-// Removes the element at the given index (shifts all the existing elements to the left)
+/*
+ * Removes the element at the given index (shifts all the existing elements to the left)
+ * @param v Pointer to the Vector
+ * @param index Index of the element to remove
+ */
 void vector_erase(Vector *v, size_t index);
 
-// Resets the vector size to 0 while keeping the existing capacity
+/*
+ * Resets the vector size to 0 while keeping the existing capacity
+ * @param v Pointer to the Vector
+ */
 void vector_clear(Vector *v);
 
-// Shrinks the vector capacity to make the current size
+/*
+ * Shrinks the vector capacity to make the current size
+ * @param v Pointer to the Vector
+ */
 void vector_shrink_to_fit(Vector *v);
 
 
 
 /* Status */
 
-// Returns the current size of the vector
+/*
+ * Returns the current size of the vector
+ * @param v Pointer to the Vector
+ */
 size_t vector_size(Vector *v);
 
-// Returns 1 if the vector is empty, else returns 1
+/*
+ * Returns 1 if the vector is empty, else returns 1
+ * @param v Pointer to the Vector
+ */
 bool vector_is_empty(Vector *v);
 
 #endif
